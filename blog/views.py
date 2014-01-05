7d24f7563f49a9e4bdd60dd64321979ba56ae499
canadien91 <nefedov.alexander91@yandex.ru>
2014-01-05 14:29:33 +0400

    [Blog] Views Class-based views are implemented

--- blog/views.py
+++ blog/views.py
@@ -7,36 +7,17 @@ from django.views import generic
 
 from models import Post
 
-# Create your views here.
-
-def index(request):
-	post_list = Post.objects.order_by('-date')[:10]
-	months = {1: 'January', 
-		2: 'February', 
-		3: 'March', 
-		4: 'April', 
-		5: 'May', 
-		6: 'June', 
-		7: 'July', 
-		8: 'August', 
-		9: 'September', 
-		10: 'October', 
-		11: 'November', 
-		12: 'December'}
-	moment = timezone.now()
-	today = str(moment.day) + ' ' + months[moment.month] + ' ' + str(moment.year)
-	context = {'post_list':post_list, 'today': today}
-	return render(request, 'blog/index.html', context)
-
-def post(request, post_id):
-	pass
-
 class BlogView(generic.ListView):
 	template_name = 'blog/index.html'
 	context_object_name = 'post_list'
 
 	def get_queryset(self):
 		return Post.objects.order_by('-date')[:10]
+
+	def get_context_data(self, **kwargs):
+		context = super(BlogView, self).get_context_data(**kwargs)
+		context['moment'] = timezone.now()
+		return context
 
 class PostView(generic.DetailView):
 	model = Post
