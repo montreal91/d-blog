0fc0ccdc5eb111ab1f19bc30f9aa741c65c588da
canadien91 <nefedov.alexander91@yandex.ru>
2014-01-08 19:06:24 +0400

    [Blog] Editing views. Editing views are implemented, but not all of them are properly working yet.

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     url(r'^$', views.BlogView.as_view(), name='index'),
     url(r'^create/$', views.CreatePostView.as_view(), name='create'),
     url(r'^update/(?P<pk>\d+)/', views.UpdatePostView.as_view(), name='update'),
-    url(r'^delete/(?P<pk>\d+)/', views.DeleteView.as_view(), name='delete'),
+    url(r'^delete/(?P<pk>\d+)/', views.DeletePostView.as_view(), name='delete'),
     url(r'(?P<pk>\d+)/', views.PostView.as_view(), name='post'),
 )
