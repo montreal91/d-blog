dc5aefdfc8a427d3c672b0ea24eb0a0e3d614162
canadien91 <nefedov.alexander91@yandex.ru>
2014-01-08 18:43:35 +0400

    [Blog] "

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,9 @@ import json
 
 from django.utils import timezone
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from models import Post
 
@@ -20,11 +21,21 @@ class AjaxResponseMixin(object):
 
 	def form_invalid(self, form):
 		response = super(AjaxResponseMixin, self).form_invalid(form)
-
 		if self.request.is_ajax():
 			return self.render_to_json_response(form.errors, status=400)
 		else:
 			return response
+
+	def form_valid(self, form):
+		response = super(AjaxResponseMixin, self).form_valid(form)
+		if self.request.is_ajax():
+			data = {
+				'pk': self.object.pk,
+			}
+			return self.render_to_json_response(data)
+		else:
+			return response
+
 
 class BlogView(AjaxResponseMixin, CreateView):
 	template_name = 'blog/index.html'
@@ -44,3 +55,15 @@ class BlogView(AjaxResponseMixin, CreateView):
 class PostView(DetailView):
 	model = Post
 	template_name = 'blog/post.html' 
+
+class CreatePostView(CreateView):
+	model = Post
+	fields = ['title', 'text']
+
+class UpdatePostView(UpdateView):
+	model = Post
+	fields = ['title', 'text']
+
+class DeletePostView(DeleteView):
+	model = Post
+	success_url = reverse_lazy('index')
